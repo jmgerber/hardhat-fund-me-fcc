e44c6837302ef1b18bc674d5eb58a407da5ccfc7
Jean-Michel <jmgerber57@gmail.com>
2022-06-29 00:05:16 +0200

    Initial commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+// Pragma
+pragma solidity ^0.8.0;
+
+// Imports
+import "./PriceConverter.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// Error codes
+error FundMe__NotOwner();
+
+//Interfaces, Libraries, Contracts
+
+/** @title A contract for crowd funding
+ * @author Jean-Michel Gerber
+ * @notice This contract si to demo a sample funding contract
+ * @dev This implements price feeds as our library
+ */
+contract FundMe {
+  // Type declarations
+  using PriceConverter for uint256;
+
+  // State variables
+  mapping(address => uint256) private s_addressToAmountFunded;
+  address[] private s_funders;
+  address private immutable i_owner;
+  uint256 public constant MINIMUM_USD = 50 * 1e18;
+  AggregatorV3Interface private s_priceFeed;
+
+  modifier onlyOwner() {
+    if (msg.sender != i_owner) {
+      revert FundMe__NotOwner();
+    }
+    _;
+  }
+
+  // constant et immutable permettent d'économiser du gaz pour des variables qui ne changent pas
+  constructor(address priceFeedAddress) {
+    i_owner = msg.sender;
+    s_priceFeed = AggregatorV3Interface(priceFeedAddress);
+  }
+
+  receive() external payable {
+    fund();
+  }
+
+  fallback() external payable {
+    fund();
+  }
+
+  /** @notice This function funds this contract
+   */
+  function fund() public payable {
+    require(
+      msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+      "50 USD minimum"
+    );
+    s_funders.push(msg.sender);
+    s_addressToAmountFunded[msg.sender] = msg.value;
+  }
+
+  function withdraw() public onlyOwner {
+    for (
+      uint256 funderIndex = 0;
+      funderIndex < s_funders.length;
+      funderIndex++
+    ) {
+      address funder = s_funders[funderIndex];
+      s_addressToAmountFunded[funder] = 0;
+    }
+
+    // reset the array
+    s_funders = new address[](0);
+    // transfer
+    // payable(msg.sender).transfer(address(this).balance);
+    // send
+    // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+    // require(sendSuccess, "Send failed");
+    // call
+    (bool callSuccess, ) = payable(msg.sender).call{
+      value: address(this).balance
+    }("");
+    require(callSuccess, "Call failed");
+  }
+
+  function cheaperWithdraw() public payable onlyOwner {
+    address[] memory funders = s_funders;
+    // mappings can't be in memory, sorry!
+    for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+      address funder = funders[funderIndex];
+      s_addressToAmountFunded[funder] = 0;
+    }
+    s_funders = new address[](0);
+    // payable(msg.sender).transfer(address(this).balance);
+    (bool success, ) = i_owner.call{value: address(this).balance}("");
+    require(success);
+  }
+
+  function getOwner() public view returns (address) {
+    return i_owner;
+  }
+
+  function getFunder(uint256 index) public view returns (address) {
+    return s_funders[index];
+  }
+
+  function getAddressToAmountFunded(address funder)
+    public
+    view
+    returns (uint256)
+  {
+    return s_addressToAmountFunded[funder];
+  }
+
+  function getPriceFeed() public view returns (AggregatorV3Interface) {
+    return s_priceFeed;
+  }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,49 @@
+require("dotenv").config()
+
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+  // solidity: "0.8.8",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 6,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0,
+    },
+  },
+}
